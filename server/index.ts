caf6ef907570fd8960454f1b81acaaf5f144c5ac
Matheus Murbach <matheus.murbach@divam.com.br>
2025-05-04 09:19:12 -0300

    uow

--- server/index.ts
+++ server/index.ts
@@ -109,15 +109,16 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5001;
+  const port = process.env.PORT;
   server.listen(
     {
-      port,
+      port: port,
       host: "0.0.0.0",
       reusePort: true,
     },
     () => {
-      log(`serving on port ${port}`);
+      const environment = app.get("env");
+      log(`Server running in ${environment} mode on port ${port}`);
     }
   );
 })();
